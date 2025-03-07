676b8cc40cb181a5ffc2c91f52a39caa5249d619
Shota0714 <u.shota0714@gmail.com>
2025-03-07 15:41:42 -0500

    add js

--- lab-2/js/script.js
+++ lab-2/js/script.js
@@ -2,11 +2,15 @@ const output = document.getElementById('output');
 
 const toyCar1 = {
     brand: "Hot Wheels",
-    model: "Ferrari",
-    color: "red",
-    year: 2019,
-    description() {
-        let description = `This is a ${this.color} ${this.year} ${this.brand} ${this.model}.`;
-        return description;
-    }
-}
+    model: "Nissan Skyline 2000GT-R LBWK",
+    color: "Pearl Magenta",
+    year: 2025,
+    series: "HW J-Imports",
+    baseColor: "Black",
+    baseType: "Plastic",
+    windowColor: "Smoke Tint",
+    wheelType: "OH5SP",
+    country: "Malaysia"
+};
+
+output.textContent = `This toy car is a ${toyCar1.year} ${toyCar1.brand} ${toyCar1.model} in ${toyCar1.color}. It is part of the ${toyCar1.series} series, made in ${toyCar1.country}.This car has a ${toyCar1.baseColor} ${toyCar1.baseType} base, ${toyCar1.windowColor} windows, and ${toyCar1.wheelType} wheels.`;
